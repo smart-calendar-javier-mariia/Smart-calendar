f9899a86ecb684342126d3dc50ea5cc4d0bd56e8
Javier <javierbuelvas@Javiers-MacBook-Pro.local>
2019-02-20 13:47:48 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,16 +27,16 @@ let event2 = {
   date:{
     year: 2019,
     month: 02,
-    day: 06
+    day: 10
   },
-  title: `Class Session`,
+  title: `Deliverable Due`,
   class: `INTR2012 INTERFACE DEVELOPMENT II`,
   startTime: `12:00`,
-  endTime: `15:00`,
-  repeats: true,
-  howOften: `Every Wednesday`,
-  reminder: `1 hour`,
-  completed: true,
+  endTime: null,
+  repeats: false,
+  howOften: null,
+  reminder: `1 day`,
+  completed: false,
   eventCategory: `School`,
   eventNotes: `Lab: Organization Interface`,
   eventCreated: {
@@ -45,4 +45,28 @@ let event2 = {
     day: 06
   },
   createdBy: `Javier`
+}
+
+let event3 = {
+  date:{
+    year: 2019,
+    month: 02,
+    day: 9
+  },
+  title: `Meeting`,
+  class: `INTR2012 INTERFACE DEVELOPMENT II`,
+  startTime: `13:30`,
+  endTime: `15:00`,
+  repeats: false,
+  howOften: null,
+  reminder: `1 hour`,
+  completed: true,
+  eventCategory: `School`,
+  eventNotes: `Work on Lab: Organization Interface`,
+  eventCreated: {
+    year: 2019,
+    month: 02,
+    day: 09
+  },
+  createdBy: `Mariia`
 }
