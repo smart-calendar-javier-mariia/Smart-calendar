6513c415221ad5bac2dcec1068f4433dc7a90e7d
Mariia <marieshpu@gmail.com>
2019-02-20 13:47:02 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -46,3 +46,51 @@ let event2 = {
   },
   createdBy: `Javier`
 }
+
+let event5 = {
+  date:{
+    year: 2019,
+    month: 02,
+    day: 20
+  },
+  title: `After class meeting`,
+  class: `INTR2012 INTERFACE DEVELOPMENT II`,
+  startTime: `15:20`,
+  endTime: `16:20`,
+  repeats: false,
+  howOften: null,
+  reminder: `1 hour`,
+  completed: false,
+  eventCategory: `School`,
+  eventNotes: `Work on group project`,
+  eventCreated: {
+    year: 2019,
+    month: 02,
+    day: 20
+  },
+  createdBy: `Javier`
+}
+
+let event4 = {
+  date:{
+    year: 2019,
+    month: 02,
+    day: 20
+  },
+  title: `In class lab`,
+  class: `INTR2012 INTERFACE DEVELOPMENT II`,
+  startTime: `13:00`,
+  endTime: `14:00`,
+  repeats: true,
+  howOften: `Every Wednesday`,
+  reminder: `1 hour`,
+  completed: true,
+  eventCategory: `School`,
+  eventNotes: `Work on in class lab`,
+  eventCreated: {
+    year: 2019,
+    month: 02,
+    day: 20
+  },
+  createdBy: `Mariia`
+}
