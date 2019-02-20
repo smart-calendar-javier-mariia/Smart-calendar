3eadc81fdd49917529fac7538281532bd9a3ac57
Javier <javierbuelvas@Javiers-MacBook-Pro.local>
2019-02-20 13:39:11 -0500

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let eventOrTask = {
+  date:{
+    year: 2019,
+    month: 02,
+    day: 06
+  },
+  title: `Class Session`,
+  class: `INTR2012 INTERFACE DEVELOPMENT II`,
+  startTime: `12:00`,
+  endTime: `15:00`,
+  repeats: true,
+  howOften: `Every Wednesday`,
+  reminder: `1 hour`,
+  completed: true,
+  eventCategory: `School`,
+  eventNotes: `Lab: Organization Interface`,
+  eventCreated: {
+    year: 2019,
+    month: 02,
+    day: 06
+  },
+  createdBy: `Javier`
+}
